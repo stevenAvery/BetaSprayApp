02a5c4357d5cc3eec8471bb402dcbeb8d4b3469e
savery <14942732+stevenAvery@users.noreply.github.com>
2022-03-18 18:42:46 -0400

    Page loaders don't show spinner for first 750ms, to improve precieved load times

--- src/_11ty/shortcodes.js
+++ src/_11ty/shortcodes.js
@@ -26,23 +26,34 @@ module.exports = {
             return getIcon(icon, options);
         },
         loading: (options = {}) => {
+            // Show loader while this condiiton is true
             const whileCondition = options['while'] ?? 'true';
+
+            // Show a loading message next to the spinner
             const message = options['message'] ?? '';
             const messageMarkup = message !== '' 
                 ? `<span class='ml-2 text-base text-slate-500'>${message}</span>` 
                 : '';
             
+            // Delay showing the spinner for a certain number of milliseconds
+            // Eg. showing a spinner on quick page loads can make the user percieve a slower load
+            const delayShow = options['delayShow'] ?? 0;
+
             return `
-            <div
-                x-show="${whileCondition}" 
-                class="w-full h-full flex flex justify-center items-center">
-                <span class="
-                    animate-spin ease-linear 
-                    rounded-full border-[3px] border-slate-400 border-r-transparent 
-                    h-6 w-6">
-                </span>
-                ${messageMarkup}
-            </div>
+                <div
+                    x-data="{
+                        delayedEnough: false
+                    }"
+                    x-show="${whileCondition} && delayedEnough" 
+                    x-init="setTimeout(() => { delayedEnough = true }, ${delayShow})"
+                    class="w-full h-full flex flex justify-center items-center">
+                    <span class="
+                        animate-spin ease-linear 
+                        rounded-full border-[3px] border-slate-400 border-r-transparent 
+                        h-6 w-6">
+                    </span>
+                    ${messageMarkup}
+                </div>
             `;
         },
     },
